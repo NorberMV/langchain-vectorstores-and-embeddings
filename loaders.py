de15347a796f302d0f2b94498024bf80d31c2a42
Norber MV <norbermv@Norbertos-MacBook-Air.local>
2023-07-25 21:31:17 -0500

    Add .gitignore and some cleanup.

--- loaders.py
+++ loaders.py
@@ -1,27 +1,29 @@
-# Different loader examples
+# Just two different simple loader examples
 import os
 import openai
 from langchain.document_loaders import PyPDFLoader
-
-
-openai.api_key  = os.environ['OPENAI_API_KEY']
-# 1.This first example in how to load data from
-# a PDF file
-# this, _ = os.path.split(__file__)
-# file = os.path.join(
-#     this,
-#     'data_docs',
-#     'MachineLearning-Lecture01.pdf'
-# )
-# # Load the pdf
-# loader = PyPDFLoader(file)
-# pages = loader.load()
-
-# 2.Loading content from url's
 from langchain.document_loaders import WebBaseLoader
 
+openai.api_key  = os.environ['OPENAI_API_KEY']
+
+# 1.This first example in how to load data from
+# a PDF file
+this, _ = os.path.split(__file__)
+file = os.path.join(
+    this,
+    'data_docs',
+    'MachineLearning-Lecture01.pdf'
+)
+# Load the pdf
+loader = PyPDFLoader(file)
+pages = loader.load()
+
+# 2.Loading content from url's
+url = (
+    "https://developer.shotgridsoftware.com/tk-core/"
+    "overview.html#what-is-the-toolkit-platform"
+)
 loader = WebBaseLoader(
-    "https://developer.shotgridsoftware.com/tk-core/overview.html#what-is-the-toolkit-platform"
 )
 docs = loader.load()
 print(docs[0].page_content[:500])
--- vectorstores/vectorstores.py
+++ vectorstores/vectorstores.py
@@ -3,21 +3,19 @@ import os
 from langchain.document_loaders import PyPDFLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 
-this, _ = os.path.split(__file__)
-
 
 # # Add our third-party packages to sys.path. We've created a zip file because some of the file paths
 # # are pretty long. We're also normalizing the path or we're getting import errors.
 
 files_root = os.path.normpath(
     os.path.join(
-        this,  # ./langchain-with-your-data/vectorstores
+        os.path.split(__file__),  # ./langchain-with-your-data/vectorstores
         os.pardir,  # ./langchain-with-your-data
         "data_docs",  # ./data_docs
     )
 )
-print(files_root)
-# Load PDF
+
+# Load PDFs
 loaders = [
     # Duplicate documents on purpose - messy data
     PyPDFLoader(os.path.join(files_root, "MachineLearning-Lecture01.pdf")),

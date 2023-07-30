45fe29bb962fb522185087734a57183514134e42
Norber MV <norbermv@Norbertos-MacBook-Air.local>
2023-07-30 07:57:50 -0500

    Fix the chromadb client as the old method was deprecated.

--- vectorstores/vectorstores.py
+++ vectorstores/vectorstores.py
@@ -4,7 +4,9 @@ from langchain.document_loaders import PyPDFLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.vectorstores import Chroma
 import chromadb
-from embeddings import embedding
+from langchain.embeddings.openai import OpenAIEmbeddings
+
+embedding = OpenAIEmbeddings()
 
 # ! pip install chromadb
 # # Add our third-party packages to sys.path. We've created a zip file because some of the file paths
@@ -35,22 +37,23 @@ text_splitter = RecursiveCharacterTextSplitter(
     chunk_size = 1500,
     chunk_overlap = 150
 )
-
+# len(splits[0].page_content)
+# Out[2]: 1499
 splits = text_splitter.split_documents(docs)
 print(len(splits))
 
-# Vectorstores
+
 persist_directory = os.path.join(os.path.dirname(__file__), 'chroma')
-client = chromadb.PersistentClient(path="db_metadata_v5")
+client = chromadb.PersistentClient(path=persist_directory)
+# Create the Vectorstore
 vectordb = Chroma.from_documents(
     client=client,
     documents=splits,
     embedding=embedding,
-    persist_directory=persist_directory
 )
 print(vectordb._collection.count())
 
 # Similarity Search
 question = "is there an email i can ask for help"
-docs = vectordb.similarity_search(question,k=3)
-len(docs)
+docs = vectordb.similarity_search(question, k=3)
+print(len(docs))

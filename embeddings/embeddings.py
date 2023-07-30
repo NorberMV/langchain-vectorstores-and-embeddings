2c3194bdc93755cb0f76419985586eb1ac501d3b
Norber MV <norbermv@Norbertos-MacBook-Air.local>
2023-07-30 07:56:33 -0500

    Ignore __pycache__ files.

--- embeddings/embeddings.py
+++ embeddings/embeddings.py
@@ -9,3 +9,10 @@ sentence3 = "the weather is ugly outside"
 embedding1 = embedding.embed_query(sentence1)
 embedding2 = embedding.embed_query(sentence2)
 embedding3 = embedding.embed_query(sentence3)
+
+# Comparing this we can see the difference between embeddings
+# import numpy as np
+# np.dot(embedding1, embedding2)
+# Out[3]: 0.9631853877103519
+# np.dot(embedding1, embedding3)
+# Out[4]: 0.770999765129468
